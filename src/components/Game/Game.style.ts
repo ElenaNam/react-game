8ae71e9fbf1bbc9622e9ef1c2e81078f6fe3cade
ElenaNam <68644379+ElenaNam@users.noreply.github.com>
2021-03-03 13:29:51 +0400

    feat: add the end of the game

--- src/components/Game/Game.style.ts
+++ src/components/Game/Game.style.ts
@@ -1,12 +1,19 @@
 import CSS from "csstype";
 
 export const root: CSS.Properties = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+};
+
+export const field: CSS.Properties = {
   margin: "0 auto",
   display: "flex",
   maxWidth: "600px",
   maxHeight: "600px",
-  minHeight: "300px",
-  minWidth: "300px",
+  /*   minHeight: "300px",
+  minWidth: "300px", */
   width: "100%",
   height: "100%",
   flexWrap: "wrap",
@@ -21,7 +28,6 @@ export const cell: CSS.Properties = {
   maxWidth: "200px",
   maxHeight: "200px",
   border: "1px solid",
-
   textAlign: "center",
   verticalAlign: "middle",
 };
@@ -29,6 +35,16 @@ export const cell: CSS.Properties = {
 export const text: CSS.Properties = {
   fontSize: "125px",
   color: "wheat",
+  display: "flex",
+  margin: "0 auto",
+  justifyContent: "center",
+  alignItems: "center",
+};
+export const message: CSS.Properties = {
+  position: "absolute",
+  top: "10px",
+  fontSize: "60px",
+  color: "tomato",
   display: "flex",
   margin: "0 auto",
   justifyContent: "center",
--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { root, cell, text } from "./Game.style";
+import { root, field, cell, text, message } from "./Game.style";
 import lineWin from "../../assets/data/lineWin";
+import { Container } from "@material-ui/core";
 
 interface GameProps {
   name: "123";
@@ -33,23 +34,42 @@ class Game extends React.Component<GameProps, GameState> {
     const innerCell = !(this.state.count % 2) ? "X" : "O";
     let win = "";
 
-    for (let i = 0; i < this.state.cells.length; i++) {
+    for (let i = 0; i < this.lineWin.length; i++) {
       const line = this.lineWin[i];
       if (
         this.state.cells[line[0]] === innerCell &&
         this.state.cells[line[1]] === innerCell &&
         this.state.cells[line[2]] === innerCell
       ) {
+        //console.log(line);
         win = `${innerCell} win!`;
         this.setState({ isEnd: true, winMessage: win });
       }
     }
+
     return;
+  };
+
+  resetGame = (): void => {
+    //console.log("reset");
+    //console.log(this.state.isEnd);
+    if (!this.state.isEnd) return;
+    setTimeout(() => {
+      console.log("игра окончена");
+      this.setState({
+        cells: Array(9).fill(null),
+        count: 0,
+        isEnd: false,
+        winMessage: "",
+      });
+      //console.log(this.state);
+    }, 3000);
   };
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   clickHandler = (e: React.MouseEvent<HTMLElement>) => {
-    e.stopPropagation();
+    //e.stopPropagation();
+    if (this.state.isEnd) return;
     const target = +e.currentTarget.id - 1;
     const currentCells = this.state.cells;
     if (currentCells[target] === null) {
@@ -58,30 +78,46 @@ class Game extends React.Component<GameProps, GameState> {
       this.setState({ cells: currentCells });
     }
     this.isEndGame();
+    //console.log(this.state.isEnd);
   };
+  componentDidMount() {
+    //console.log("componentDidMount");
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+    console.log(this.state.cells);
+    //this.state.isEnd ? this.setState({ cells: Array(9).fill(null) }) : false;
+    this.resetGame();
+    console.log("componentDidUpDate");
+    console.log(this.state);
+  }
 
   render() {
     const cells = this.state.cells;
     const isEnd = this.state.isEnd;
     const winMessage = this.state.winMessage;
-    return (
-      <div style={root}>
-        {isEnd ? <div>{winMessage}</div> : false}
-        {cells.map((item, i) => {
-          return (
-            <div
-              style={cell}
-              onClick={this.clickHandler}
-              key={i + 1}
-              id={(i + 1).toString()}
 
-              //ref={() => this.myRef}
-            >
-              <span style={text}>{this.state.cells[i]}</span>
-            </div>
-          );
-        })}
-      </div>
+    return (
+      <Container maxWidth="md" style={root}>
+        {console.log("isEnd render" + isEnd)}
+        {isEnd ? <div style={message}>{winMessage}</div> : false}
+        <div style={field}>
+          {cells.map((item, i) => {
+            return (
+              <div
+                style={cell}
+                onClick={this.clickHandler}
+                key={i + 1}
+                id={(i + 1).toString()}
+
+                //ref={() => this.myRef}
+              >
+                <span style={text}>{this.state.cells[i]}</span>
+              </div>
+            );
+          })}
+        </div>
+      </Container>
     );
   }
 }
